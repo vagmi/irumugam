894e3e08e8de2329dddd7913446cbcbd5a8f8dae
Vagmi Mudumbai <vagmimud@thoughtworks.com>
2012-07-24 18:19:31 +0530

    generate better specs for all verbs

--- lib/irumugam/spec_writer.rb
+++ lib/irumugam/spec_writer.rb
@@ -2,22 +2,24 @@ require 'rspec'
 module Irumugam
   class SpecWriter
     def self.create_example_groups
-      #Process GET Requests
       ServiceRegistry.instance.services.values.each do |service|
+
         eg = RSpec::Core::ExampleGroup.describe("#{service.name} specs") do
           before :all, &(service.before_blocks[:all]) if service.before_blocks[:all]
           before :each, &(service.before_blocks[:each]) if service.before_blocks[:each]
           after :all, &(service.after_blocks[:all]) if service.after_blocks[:all]
           after :each, &(service.after_blocks[:each]) if service.after_blocks[:each]
+
           ServiceRegistry.instance.paths_for(service.name).each do |contract|
-            it "should succeed for GET #{contract.path}" do
-              response = HTTParty.get(contract.test_host + contract.path, :query=>contract.params) 
+            it "#{contract.method} #{contract.path} should return with #{contract.contract_status}" do
+              response = HTTParty.send(contract.method.downcase.to_sym,contract.test_host + contract.path, :query=>contract.params) 
               response.code.should==contract.contract_status
               response.body.should==contract.contract_body
             end
           end
         end
         eg.register
+
       end
     end
   end
